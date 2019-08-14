8cc9f32e1fc7865312c5886d3f6103ddf900dd92
Tyler <tcloetingh@gmail.com>
2019-08-14 19:19:25 +0000

    aws upload

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,7 +4,7 @@ class LessonsController < ApplicationController
 
   private
 
-  helper_method
+  helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
