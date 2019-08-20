03b2400a25ee58049bc0089e2032b12c8542218e
Tyler <tcloetingh@gmail.com>
2019-08-20 23:33:13 +0000

    ranked-model gem

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+
   def show
   end
 
@@ -8,4 +11,13 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+ 
+  def require_authorized_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'Unauthorized to view this lesson'
+    end
+  end
+
+
 end
